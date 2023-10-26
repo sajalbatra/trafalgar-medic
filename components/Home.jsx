4d6ecb07b45488854eae0acaa19a1da8985869e8
Sajal Batra <sajalbatra7@gmail.com>
2023-10-26 16:20:26 +0530

    Header and Home Page added

--- components/Home.jsx
+++ components/Home.jsx
@@ -2,14 +2,14 @@ import homepagepng from "@/public/homepageimg.png"
 import Image from "next/image"
 const Home = () => {
   return (
-    <div className="flex justify-around mt-20">
+    <div className="flex flex-wrap justify-around mt-20">
         {/* left div */}
         <div className="ml-[4rem]  w-[28rem]" >
             <h1 className="text-5xl text-black font-Mulish">Virtual healthcare for you</h1>
             <p className="mt-6 text-xl text-gray-500" >Trafalgar provides progressive, and affordable 
 healthcare, accessible on mobile and online 
 for everyone</p>
-        <button className="w-56 h-16 text-white bg-[#458FF6] rounded-[55px] mt-6 font-bold">Consult today</button>
+        <button className="w-56 h-16 text-white bg-[#458FF6] rounded-[55px] mt-6 ">Consult today</button>
         </div>
         <Image
         src={homepagepng}
