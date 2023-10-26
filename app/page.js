39ef35e4b1a94ac29f55db51679fbc29e29c2ef7
Sajal Batra <sajalbatra7@gmail.com>
2023-10-26 19:31:55 +0530

    Service Section  added

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import Home from "@/components/Home"
+import Services from "@/components/Services"
 const page = () => {
   return (
     <div>
       <Home/>
+      <Services/>
     </div>
   )
 }
---
+++ components/Services.jsx
@@ -0,0 +1,14 @@
+const Services = () => {
+  return (
+    <div>
+        {/* heading */}
+        <div  className="text-center">
+        <h1 className="mt-20 text-4xl font-bold" >Our services</h1>
+        {/* <hr className="flex items-center justify-center w-[56px] rounded-sm h-1 bg-black"/>  */}
+        <p className="mt-3 text-xl text-[#7D7987] mr-36 ml-36 ml- ml">We provide to you the best choiches for you. Adjust it to your health needs and make sure your undergo treatment with our highly qualified doctors you can consult with us which type of service is suitable for your health</p>
+        </div>
+    </div>
+  )
+}
+
+export default Services
